cfb7ec1d36c129833d05ea84bd45a18bdc8166a3
remenyo <remenyo@gmail.com>
2024-04-28 10:51:10 +0200

    removed duplicate apt sources

--- server/install-docker.sh
+++ server/install-docker.sh
@@ -12,11 +12,4 @@ echo \
     sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update -y
 
-# Add the repository to Apt sources:
-echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-sudo apt-get update -y
-
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
