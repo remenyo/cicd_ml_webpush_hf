8f66b900ff8be39b6031812087a710a82ee0707e
remenyo <remenyo@gmail.com>
2024-05-09 16:56:37 +0200

    added urlBase64ToUint8Array(base64String)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -158,6 +158,21 @@ const uploadImage = async () => {
 const publicKey =
   "BNhW-5fIrYsMSfDjOGpMgiBD1w7qJrPB-kHbGmCOrBqOSDImnxmcUlrtbs8WQmuzdcxpbjKVJOOE2VQL5DqNn0E";
 
+// Web-Push
+// Public base64 to Uint
+function urlBase64ToUint8Array(base64String) {
+  var padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  var base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
+
+  var rawData = window.atob(base64);
+  var outputArray = new Uint8Array(rawData.length);
+
+  for (var i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 async function subscribeToPushNotifications() {
   // Check for service worker support
   if (!("serviceWorker" in navigator)) {
